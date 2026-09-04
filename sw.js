2ecc6b25b5fdfebd34e4dff404ab7ba5041cb271
zie <zieezie23@gmail.com>
2026-09-04 22:45:34 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='checklist-simulasi-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html')))));
